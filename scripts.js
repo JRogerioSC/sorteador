b0f7cafe36980b15191e8223544e4dfdbf1a4100
JRogerioSC <jo-rogerio@hotmail.com>
2024-05-08 15:58:59 -0300

    if e else adicionado

--- scripts.js
+++ scripts.js
@@ -3,7 +3,24 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    alert(result)
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+
+        alert(result)
+
+    } else{
+
+        alert("ERRO ! o MINIMO tem de ser MENOR que o MAXIMO.")
+        
+
+    }
+
+       
+    
+        
+        
+
+   
+    
 }
